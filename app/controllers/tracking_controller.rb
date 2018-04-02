4d1be6ab7baf2a29c2e55160ba3542c73a814434
Edward Woodcock <edward@getdinghy.com>
2018-04-02 18:37:03 +0100

    Baseline

---
+++ app/controllers/tracking_controller.rb
@@ -0,0 +1,12 @@
+class TrackingController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+  end
+
+  def create
+    ap request.parameters
+    # render json: { "result": "ok" }
+    render json: {}
+  end
+end
