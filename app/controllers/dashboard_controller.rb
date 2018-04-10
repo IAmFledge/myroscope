7a488a8179c202703eb9451436e455908bf462d7
Edward Woodcock <edward@getdinghy.com>
2018-04-11 00:45:50 +0100

    Partial

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @geo = GeoDataPoint.first
+  end
 end
--- app/models/geo_data_point.rb
+++ app/models/geo_data_point.rb
@@ -1,2 +1,7 @@
 class GeoDataPoint < ApplicationRecord
+  enum motion: [:stationary, :walking, :running, :cycling, :driving]
+  enum battery_state: [:unknown, :unplugged, :charging, :full]
+
+  belongs_to :user
+  belongs_to :geo_segment
 end
